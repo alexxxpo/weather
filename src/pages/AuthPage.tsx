1d306cb1991c2215ecd559d5588db4fbb294af48
alexxxpo <59997986+alexxxpo@users.noreply.github.com>
2022-08-21 23:00:02 +0500

    change text auth page

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function AuthPage() {
   return (
-    <div className='pt-24 px-10 pb-10'>AuthPage</div>
+    <div className='pt-24 px-10 pb-10'>AuthPage Reistration and authorization</div>
   )
 }
