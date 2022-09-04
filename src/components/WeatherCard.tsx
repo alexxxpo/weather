c6eda5faccd7c48a0ea6a41a64c3a72db0a3eb60
alexxxpo <59997986+alexxxpo@users.noreply.github.com>
2022-09-04 23:41:22 +0500

    обработка названий российских городов

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,29 +1,38 @@
 import { useGetCurrentWeatherQuery } from '../redux/weather/weather.api'
+import russianCities from '../cities_ru_en.json'
+
 
 interface IWeatherCardProps {
     query?: string
 }
 
-export default function WeatherCard(props:IWeatherCardProps ) {
-
-    const searchQuery = props.query ? props?.query.length < 3 ? 'auto:ip' : props?.query :'auto:ip'
-    const { isLoading, isError, data } = useGetCurrentWeatherQuery(searchQuery, {skip: searchQuery.length < 3})
+export default function WeatherCard(props: IWeatherCardProps) {
+    
+    const checkCity = (city: string, list: string[][], country: string = 'Russia'): string => {
+        return list.reduce((res: string, item: string[]): string => {
+            if (item[1] === city) res = item[0]
+            return res
+        }, '')
+    }
+    const searchQuery = props.query ? props?.query.length < 3 ? 'auto:ip' : props?.query : 'auto:ip'
+    const { isLoading, isError, data } = useGetCurrentWeatherQuery(searchQuery, { skip: searchQuery.length < 3 })
 
     const wind: string = data?.current.wind_kph ? ((data?.current.wind_kph / 3600) * 1000).toFixed(1).toString() : '-'
     const tempC: string = data?.current.temp_c ? (data?.current.temp_c).toString() : '-'
     const feelslike: string = data?.current.feelslike_c ? data?.current.feelslike_c.toString() : '-'
     const pres: string = data?.current.pressure_mb ? (data?.current.pressure_mb * 0.750064).toFixed(1).toString() : '-'
     const country = data ? data?.location.country === 'Russia' ? 'Россия' : data?.location.country : '-'
-    const city: string = data?.location.name ? data?.location.name : '-'
+    const city: string = data ? checkCity(data?.location.name, russianCities) : ''
     const condition: string = data?.current.condition.text ? data?.current.condition.text : '-'
     const conditionImgSrc: string = data?.current.condition.icon ? data?.current.condition.icon : ''
+
 
     if (isError) return (
         <div className='weather_card'>
             <div className='error'>Что-то пошло не так</div>
         </div>
     )
-    
+
     if (isLoading) return (
         <div className='weather_card'>
             <div className='loader'>Загрузка...</div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,18 @@
 import WeatherCard from '../components/WeatherCard'
 import { useAppSelector } from '../hooks/reduxHooks'
 import useDebounce from '../hooks/useDebounce'
+import russianCities from '../cities_ru_en.json'
 
 export default function HomePage() {
 
     const search = useAppSelector((state) => state.setSearchWeather.search)
-    const debounced = useDebounce(search, 500)    
+    const debounced = useDebounce(search, 500)
+    console.log(russianCities);
+    
 
     return (
         <div className="home container pt-24 px-10 pb-10 mx-auto">
-            <WeatherCard query={debounced}/>                    
+            <WeatherCard query={debounced} />
         </div>
-
     )
 }
